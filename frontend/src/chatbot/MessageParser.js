0cdc56912e009798df72b21c696d58c6e07d23ee
Urwa Ihtesham <urwaihtesham@gmail.com>
2024-07-04 13:51:00 +0500

    chatbot: added dropdown menu

--- frontend/src/chatbot/MessageParser.js
+++ frontend/src/chatbot/MessageParser.js
@@ -1,22 +1,3 @@
-
-
-/*const MessageParser = ({ children, actions }) => {
-  const parse = (message) => {
-    console.log(message);
-  };
-
-  return (
-    <div>
-      {React.Children.map(children, (child) => {
-        return React.cloneElement(child, {
-          parse: parse,
-          actions: {},
-        });
-      })}
-    </div>
-  );
-};*/
-// MessageParser.js
 class MessageParser {
     constructor(actionProvider, state) {
       this.actionProvider = actionProvider;
@@ -24,15 +5,18 @@ class MessageParser {
     }
   
     parse = (message) => {
-      const { nextInputAction } = this.state; // Get current state of expected input
-  
-      // Check if there's a specific next input action expected
-      if (nextInputAction) {
-        // Use the stored function reference from the state to handle the input
-        nextInputAction(message);
+      const lowerCaseMessage = message.toLowerCase();
+
+      // Check for greetings
+      if (["hi", "hello"].includes(lowerCaseMessage)) {
+          this.actionProvider.greetUser();
       } else {
-        // Default to handling category if no specific input action is set
-        this.actionProvider.handleAnimalCategory(message);
+          // Proceed with other parsing logic
+          if (this.state.nextInputAction) {
+              this.state.nextInputAction(lowerCaseMessage);
+          } else {
+              this.actionProvider.handleAnimalCategory(lowerCaseMessage);
+          }
       }
     }
   }
