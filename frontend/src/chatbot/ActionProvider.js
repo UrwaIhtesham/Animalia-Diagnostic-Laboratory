2a6216a9b431ff65a5b5f491aaf4293629d046c4
Urwa Ihtesham <urwaihtesham@gmail.com>
2024-07-05 14:24:00 +0500

    chatbot: Added espress app in backend, Changed some questions of chatbot

--- frontend/src/chatbot/ActionProvider.js
+++ frontend/src/chatbot/ActionProvider.js
@@ -26,12 +26,24 @@ class ActionProvider {
     this.updateChatbotState(message);
   };
 
-  handleInfo = (animalType) => {
-    const infomessage = 'Enter Information about your animal (age, breed, etc).';
+  handleInfo = (animalType)=>{
+    const infomessage = 'Enter information about your animal (age, breed, etc).';
     const message = this.createChatBotMessage(infomessage);
     this.updateChatbotState(message);
-    this.state.currentAnimalType = animalType;
-    this.state.nextInputAction = this.handleSymptoms;
+    this.setState(state=> ({
+      ...state,
+      currentAnimalType:animalType,
+      nextInputAction: (info) => this.handleInfoInput(info, animalType)
+    }));
+  };
+
+  handleInfoInput = (info, animalType) => {
+    if (info.trim().split(/\s+/).length === 1) {
+      this.handleInvalidMessage("information (please provide more details)");
+    } else {
+      console.log(this.state.currentAnimalType);
+      this.handleSymptoms(animalType);
+    }
   };
 
   greetUser = () => {
@@ -41,19 +53,22 @@ class ActionProvider {
 
   handleAnimalCategory = (category) => {
     // Define valid categories
-    const validCategories = ["pets", "livestock", "poultry"];
+    const validCategories = ["pets", "pet", "livestock", "poultry"];
+    const lower = category.toLowerCase();
+    const detectedcategory = validCategories.find(validCategory => lower.includes(validCategory)
+    );
 
     // Check if the entered category is valid
-    if (validCategories.includes(category.toLowerCase())) {
-      if (category.toLowerCase() === "pets") {
+    if (detectedcategory) {
+      if (detectedcategory.toLowerCase() === "pets" || detectedcategory.toLowerCase(0 === "pet")) {
         const message = this.createChatBotMessage("Please enter your pet type (Dog, Cat, Parrot):");
         this.updateChatbotState(message);
         this.setState(state => ({ ...state, nextInputAction: this.handlePetType }));
-      } else if (category.toLowerCase() === "livestock") {
+      } else if (detectedcategory.toLowerCase() === "livestock") {
         const message = this.createChatBotMessage("Please enter your livestock type (Cow, Buffalo, Sheep, Goat):");
         this.updateChatbotState(message);
         this.setState(state => ({ ...state, nextInputAction: this.handleLiveStockType }));
-      } else if (category.toLowerCase() === "poultry") {
+      } else if (detectedcategory.toLowerCase() === "poultry") {
         const message = this.createChatBotMessage("Please enter your poultry type (Chicken):");
         this.updateChatbotState(message);
         this.setState(state => ({ ...state, nextInputAction: this.handlePoultryType }));
@@ -64,41 +79,108 @@ class ActionProvider {
     }
   };
 
-  handleSymptoms = () => {
-    const { currentAnimalType } = this.state.currentAnimalType;
-    if (!currentAnimalType) {
-      this.handleInvalidMessage("animal type");
-      return;
-    }
+  handleSymptoms = (animalType) => {
+    const symptoms = this.symptomsByAnimalType[animalType.toLowerCase()];
+    console.log("Symptom before dropdown: ",symptoms);
+      // Create a dropdown message
+      console.log("animalType:", animalType);
+      console.log("data object to pass:", { animalType: animalType });
+      const dropdownMessage = this.createChatBotMessage(
+        "Please Select a symptom:",
+        {
+          widget: "SymptomsDropdown",
+          options: symptoms.map(symptom => ({ value: symptom, label: symptom })),
+          data:{animalType:animalType}
+        }
+      );
+      this.updateChatbotState(dropdownMessage);
+    };
 
+  // handlePetType = (petType) => {
+  //   const validPets = ["dog", "dogs", "cats", "cat", "parrots", "parrot"];
+  //   const lowerPetType = petType.toLowerCase();
 
-    const symptoms = this.symptomsByAnimalType[currentAnimalType.toLowerCase()];
+  //   const detectedPetType = validPets.find(validPet => lowerPetType.includes(validPet)
+  //   );
 
-    const dropdownMessage = this.createChatBotMessage(
-      "Please select a symptom:",
-      {
-        widget: "SymptomsDropdown",
-        options: symptoms.map(symptom => ({ value: symptom, label: symptom })),
-        handleSelect: this.handleSelectedSymptom,
-        data: { animalType: currentAnimalType }
-      }
-    );
+  //   if (detectedPetType === "dogs"){
+  //     petType = "dog";
+  //     const message = "Your pet type is ${petType}";
+  //     this.createChatBotMessage()
+  //     console.log(petType);
+  //   }
 
-    this.updateChatbotState(dropdownMessage);
-  };
+  //   if (detectedPetType === "cats"){
+  //     petType = "cat";
+  //     console.log(petType);
+  //   }
+
+  //   if (detectedPetType=== "parrots"){
+  //     petType = "parrot";
+  //     console.log(petType);
+  //   }
+
+  //   if (detectedPetType) {
+  //     this.handleInfo(petType);
+  //   } else {
+  //     this.handleInvalidMessage("pet (Dog, Cat, Parrot)");
+  //   }
+  // };
 
   handlePetType = (petType) => {
-    const validPets = ["dog", "cat", "parrot"];
-    if (validPets.includes(petType.toLowerCase())) {
-      this.handleInfo(petType);
+    const validPets = ["dog", "dogs", "cat", "cats", "parrot", "parrots"];
+    const lowerPetType = petType.toLowerCase();
+
+    const detectedPetType = validPets.find(validPet => lowerPetType.includes(validPet));
+  
+    if (detectedPetType === "dogs"){
+          petType = "dog";
+          const chatmessage=this.createChatBotMessage("Your pet type is ${petType}")
+          this.updateChatbotState(chatmessage);
+          console.log(petType);
+    }
+    
+    if (detectedPetType === "cats"){
+          petType = "cat";
+          const message = "Your pet type is ${petType}";
+          const chatmessage=this.createChatBotMessage(message)
+          this.updateChatbotState(chatmessage);
+          console.log(petType);
+    }
+    
+    if (detectedPetType=== "parrots"){
+          petType = "parrot";
+          const message = "Your pet type is ${petType}";
+          const chatmessage=this.createChatBotMessage(message)
+          this.updateChatbotState(chatmessage);
+          console.log(petType);
+    }
+  
+    if (detectedPetType) {
+      this.handleInfo(detectedPetType);
     } else {
       this.handleInvalidMessage("pet (Dog, Cat, Parrot)");
     }
   };
+  
 
   handleLiveStockType = (livestocktype) => {
-    const validLivestock = ["cow", "buffalo", "sheep", "goat"];
-    if (validLivestock.includes(livestocktype.toLowerCase())) {
+    const validLivestock = ["cow", "cows", "buffalos", "buffalo", "sheeps", "sheep", "goats", "goat"];
+    const lowerLivestockType = livestocktype.toLowerCase();
+
+    const singularLivestockTypeMap = {
+      cows: "cow",
+      buffalos: "buffalo",
+      sheeps: "sheep",
+      goats: "goat"
+    };
+
+    const detectedLivestockType = validLivestock.find(validLivestock => lowerLivestockType.includes(validLivestock));
+    if (detectedLivestockType) {
+      const mappedLivestockType = singularLivestockTypeMap[detectedLivestockType] || detectedLivestockType;
+      const message = `Your Livestock type is ${mappedLivestockType}.`;
+      const chatbotMessage = this.createChatBotMessage(message);
+      this.updateChatbotState(chatbotMessage);
       this.handleInfo(livestocktype);
     } else {
       this.handleInvalidMessage("livestock (Cow, Buffalo, Sheep, Goat)");
