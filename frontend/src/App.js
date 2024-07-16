1a4ff1384a0cb9a402d22b91d3d26fc8f7de9966
NoorFatyma <noorahfatyma@outlook.com>
2024-07-16 18:49:02 +0500

    appointment_frontend: made appointment.js and appointment.css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,15 @@
 import React, { useState, useRef } from "react";
+
 //import Chatbot from 'react-chatbot-kit';
 //import 'react-chatbot-kit/build/main.css';
 //import config from './chatbot/config';
 //import ActionProvider from './chatbot/ActionProvider';
 //import MessageParser from './chatbot/MessageParser';
-import Home from './home/home';
+//import Home from './home/home';
 //import LandingPage from './Components/LandingPage/Landingpage'
-
+import Appointment from "./Appointment/Appointment.js";
 import './App.css'
+
 
 function App() {
   const [state] = useState({});
@@ -17,7 +19,7 @@ function App() {
 
   return (
     /*<div className="App">
-       <Chatbot
+       /*<Chatbot
           config={config}
           messageParser={MessageParser}
           actionProvider={ActionProvider}
@@ -26,9 +28,13 @@ function App() {
             height: '50px',
           }}
         />
-        
     </div>*/
-    <Home/>
+    //<Home/>
+    <div className="app">
+      <Appointment />
+    </div>
+
+    
     //<LandingPage/>
   );
 }
