53c780181977f39a225cdd8d2e03cc2191eb7e2b
UrwaIhtesham <168017683+UrwaIhtesham@users.noreply.github.com>
2024-07-18 13:39:41 +0500

    Update __init__.py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = '331680ac915f2710f1ffaeb5a268310a'
     # Import blueprints here to avoid circular imports
     #app.register_blueprint(main_bp, url_prefix='/main')
 
